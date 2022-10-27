0ee1bd65d5829001faea72c0573b20ed34bd5c15
dunnoconnor <michael.dunn.oconnor@gmail.com>
2022-10-27 12:29:39 -0400

    add one to many association

--- models/index.js
+++ models/index.js
@@ -2,6 +2,8 @@ const {Band} = require('./Band')
 const {Musician} = require('./Musician')
 
 // TODO - create one-to-many association
+Musician.belongsTo(Band);
+Band.hasMany(Musician)
 
 module.exports = {
     Band,
