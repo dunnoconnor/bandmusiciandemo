2391bd1085de02c6e8dca92b5df7c66fc70a5251
dunnoconnor <michael.dunn.oconnor@gmail.com>
2022-10-27 10:35:38 -0400

    seed database

--- seed.js
+++ seed.js
@@ -4,14 +4,14 @@ const fs = require('fs').promises //helps us get access to promises when dealing
 
 //import our database [x]
 //import the model that we are trying to import our data into [x]
-const {db} = require('./db')
+const {sequelize} = require('./db')
 const { Musician, Band } = require('./models/index')
 
 
 //write our seed function -> take our json file, create rows with our data into it
 const seed = async () => {
 
-    await db.sync({ force: true }); // clear out database + tables
+    await sequelize.sync({ force: true }); // clear out database + tables
 
     const musicianSeedPath = path.join(__dirname, 'musicians.json'); //get the path to Musician.json file
     const bandSeedPath = path.join(__dirname, 'bands.json')
@@ -20,10 +20,10 @@ const seed = async () => {
     const buffer = await fs.readFile(musicianSeedPath); //asynchronously reads the content in this file
     const bandBuffer = await fs.readFile(bandSeedPath);
 
-    const {MusiciansData} = JSON.parse(String(buffer)); // First we convert the data from buffer into a string, then we parse the JSON so it converts from string -> object
+    const {musiciansData} = JSON.parse(String(buffer)); // First we convert the data from buffer into a string, then we parse the JSON so it converts from string -> object
     const {bandsData} = JSON.parse(String(bandBuffer));
 
-    const MusicianPromises = MusiciansData.map(musician => Musician.create(musician)); //creates Musician and puts it into our Musician table
+    const MusicianPromises = musiciansData.map(musician => Musician.create(musician)); //creates Musician and puts it into our Musician table
     const BandPromises = bandsData.map(band => Band.create(band));
 
     await Promise.all(MusicianPromises); // The Promise.all() method takes an iterable of promises as an input, and returns a single Promise that resolves to an array of the results of the input promises.
--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const seed = require('./seed');
 const app = express();
 // set port to 3000
 const port = 3000;
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}));
+
+seed()
 
 app.get('/', (req,res)=>{
     res.send('hello world')
